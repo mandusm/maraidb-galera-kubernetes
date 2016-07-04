c49fcf4e5c9c1662e11884e1b4b5c46e807e7b19
Mandus Momberg <mombergm@amazon.com>
2016-07-04 12:53:43 +0200

    Update Root Password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-/usr/bin/mysqladmin password "$MYSQL_ROOT_PASSWORD"
+/usr/sbin/mysqld &> /tmp/mysql.out  &
+echo $! > /tmp/mysql.pid
+while true; do
+	/usr/bin/mysqladmin password "$MYSQL_ROOT_PASSWORD" &> /dev/null
+	if [ $? -eq 0 ]; then
+		break
+	fi
+done
 
-/usr/bin/mysqld "$@"
+kill -9 `cat /tmp/mysql.pid`
+/usr/sbin/mysqld "$@"
+
+
